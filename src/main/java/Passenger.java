4ccdc24d49c454b8fd3a031b8aa5240b8ec2fc55
Umar Twynam <umartwynam@gmail.com>
2023-04-12 09:24:08 +0100

    updated passenger class

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,14 @@
 public class Passenger {
+
+    private String name;
+    private int phoneNumber;
+    private String emailAddress;
+    private int id;
+
+    public Passenger(String name, int phoneNumber, String emailAddress, int id) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.id = id;
+    }
 }
